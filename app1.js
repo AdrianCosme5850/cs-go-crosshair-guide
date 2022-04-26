bc872eadb26ef7329023d4cfa266768b1f3ac06a
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-04-25 17:30:42 -0700

    Reformatted javascript into one file, and modified age prompt acceptable answers.

--- app1.js
+++ app1.js
@@ -1,3 +1,12 @@
-let age = prompt("Are you 18 years or older?")
-if (age.toLowerCase() == "no"){document.write("<style> .text { background-color: white; color: white} </style>"),
+function ageCheck() {let age = prompt("Are you 18 years or older?(Yes/No)")
+if (age.toLowerCase() == "no")
+{document.write("<style> .text { background-color: white; color: white} </style>"),
  document.write('<h1 style="color: white;text-align: center"> CSGO is an M-rated game.</h1>')}
+if (age.toLowerCase() =="yes"){document.write('<h1 style="color: white;text-align: center"> Welcome!</h1>')}
+else {alert("Please enter Yes or No."); ageCheck()}}
+
+ function favoriteColor(){let color = prompt("What's your favorite color?")
+ document.write("<style> h1 {color:" + color + ";}</style>")}
+
+ function userNameFunction(){let userName = prompt("What's your name?")
+ document.write("<h1>Hello " + userName + "!</h1>")}
